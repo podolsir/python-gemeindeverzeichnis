4311ed7d297387ee90977bf842dbe3421408992f
Igor Podolskiy <igor.podolskiy@vwi-stuttgart.de>
2011-06-18 19:40:29 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='gemeindeverzeichnis',
+      version='0.1',
+      py_modules=['gemeindeverzeichnis'],
+      author='Igor Podolskiy',
+      author_email='igor.podolskiy@vwi-stuttgart.de',
+      )
