d41bbc48e6cbc1b37e3dc6e2368fb52cf344bd6d
Igor Podolskiy <igor.podolskiy@gmx.de>
2024-11-25 21:41:58 +0100

    Update license information.

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(name='gemeindeverzeichnis',
       version='0.1',
       py_modules=['gemeindeverzeichnis'],
       author='Igor Podolskiy',
-      author_email='igor.podolskiy@vwi-stuttgart.de',
+      author_email='ipodo@gmx.de',
       )
